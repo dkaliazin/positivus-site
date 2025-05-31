a555b4d42cefed47fcd30acfd3019b7fb1d1a7cf
dkaliazin <dkaliazin@gmail.com>
2025-05-31 19:16:07 +0300

    start header
    starting header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import css from './Header.module.css';
+const Header = () => {
+    return (
+        <header className={css.header}>
+            <h1 className={css.headerpositivus}>Positivus</h1>
+            <ul className={css.navigation}>
+                <li className={css.navitem}>About us</li>
+                <li className={css.navitem}>Services</li>
+                <li className={css.navitem}>Use Cases</li>
+                <li className={css.navitem}>Pricing</li>
+                <li className={css.navitem}>Blog</li>
+            </ul>
+
+
+        </header>
+    )
+}
+export default Header;
