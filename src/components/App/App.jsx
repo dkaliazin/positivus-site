6fbcae7032d7c84d3e270a6ad36ed268ff8b4a7e
dkaliazin <dkaliazin@gmail.com>
2025-06-05 21:56:16 +0300

    css test

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,7 +5,8 @@ import Hero from '../Hero/Hero';
 import Services from '../Services/Services';
 import CaseStudies from '../CaseStudies/CaseStudies';
 import OurWorkingProcess from '../OurWorkingProcess/OurWorkingProcess';
-import Team from '../Team/Team';
+import Team from '../Team/Team'
+import Testimonials from '../Testimonials/Testimonials';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -16,7 +17,8 @@ function App() {
       <Services />
       <CaseStudies />
       <OurWorkingProcess />
-      <Team/>
+      <Team />
+      <Testimonials/>
     </>
   )
 }
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,19 @@
+import css from './Testimonials.module.css';
+
+const Testimonials = () => {
+    return (
+        <div className={css.testimon}>
+            <div className={css.testimonheaderbox}>
+                <h2 className={css.testimonheaderboxheader}>Testimonials</h2>
+                <p className={css.testimonheaderboxtext}>
+                    Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services
+                </p>
+            </div>
+
+            <div className={css.testimonbox}>
+1
+            </div>
+        </div>
+    )
+}
+export default Testimonials;
