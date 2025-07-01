c5b5b5ff1dedfba36ef79f418e2fa01729fca647
dkaliazin <dkaliazin@gmail.com>
2025-07-02 00:49:23 +0300

    hero use-cases

--- src/pages/UseCases/UseCases.jsx
+++ src/pages/UseCases/UseCases.jsx
@@ -1,5 +1,14 @@
+import InfoSection from "../../components/InfoSection/InfoSection";
+import infoImg from '../../assets/hero-use-cases.png'
 const UseCases = () => {
-    return <h1>UseCases</h1>;
+  return (
+    <InfoSection
+      imgSrc={infoImg}
+      alt={'Hero image'}
+      title={'Proven Success Stories'}
+      text={'See how our innovative digital marketing strategies have transformed businesses. Whether through SEO, PPC, social media, or web design, these use cases highlight the tangible impact of our work. Explore the successes and envision what we can achieve together.'}
+    />
+  );
   };
   
   export default UseCases;
