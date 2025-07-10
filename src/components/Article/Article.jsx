c714c1da63413e36e16f0e284cb2332a292b227b
dkaliazin <dkaliazin@gmail.com>
2025-07-10 22:24:10 +0300

    Update Article.jsx

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,21 @@
 import css from './Article.module.css';
 const Article = () => {
-    
+    return (
+        <section className={css.article}>
+            <div className={css.header}>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <h4></h4>
+                        <p></p>
+                    </div>
+                </div>
+                <div>
+                    
+                    
+                </div>
+            </div>
+        </section>
+    )
 }
 export default Article;
